cd989a405840a83af5b653ee66857d0fc0e08703
Matej Skalicky <matej.skalickyy@gmail.com>
2026-03-16 22:27:17 +0100

    context limiting

--- lua/plugins/ts-context.lua
+++ lua/plugins/ts-context.lua
@@ -1,3 +1,14 @@
 return {
 	"nvim-treesitter/nvim-treesitter-context",
+	config = function()
+		require("treesitter-context").setup({
+			max_lines = 10,
+			line_numbers = true,
+			multiline_threshold = 3,
+			on_attach = function(buf)
+				local ft = vim.bo[buf].filetype
+				return ft == "typescriptreact" or ft == "html" or ft == "tsx"
+			end,
+		})
+	end,
 }
