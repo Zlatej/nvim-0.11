6a49fb6a8759ceda1b852e1ac44a6281c24a0c2b
Matej Skalicky <matej.skalickyy@gmail.com>
2026-03-16 22:27:08 +0100

    treesitter update

--- lua/plugins/treesiter.lua
+++ lua/plugins/treesiter.lua
@@ -1,11 +1,23 @@
 return {
-	{ -- Highlight, edit, and navigate code
+	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
-		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-		opts = {
-			ensure_installed = {
+        branch = "master",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			local default_ensure_installed = {
+				"python",
+				"csv",
+				"css",
+				"go",
+				"java",
+				"json",
+				"objdump",
+				"rust",
+				"sql",
+				"ssh_config",
+				"xml",
+				"yaml",
 				"bash",
 				"c",
 				"diff",
@@ -21,19 +33,22 @@ return {
 				"javascript",
 				"typescript",
 				"tsx",
-			},
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = { enable = true, disable = { "ruby" } },
-			-- autotag = { enable = true },
-		},
+			}
+			local ensure_installed = vim.g.treesitter_ensure_installed
+			if ensure_installed == nil then
+				ensure_installed = default_ensure_installed
+			elseif type(ensure_installed) == "table" then
+				if vim.tbl_isempty(ensure_installed) then
+					ensure_installed = "all"
+				end
+			elseif type(ensure_installed) ~= "string" then
+				ensure_installed = default_ensure_installed
+			end
+			configs.setup({
+				ensure_installed = ensure_installed,
+				highlight = { enable = true },
+			})
+		end,
 	},
 	{
 		{
